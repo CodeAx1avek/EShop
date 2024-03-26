2b669ad11c46a7d59e5d426c9afef9adbce42db6
CodeAx1Avek <sachinkewat703@gmail.com>
2024-03-26 21:03:19 +0530

    size

--- shop/models/product.py
+++ shop/models/product.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
-    # discount = models.IntegerField(default=0)
+    size = models.CharField(max_length=50,default="")
     description = models.CharField(max_length=200, default='Best Qualtiy')
     image = models.ImageField(upload_to='products/')
 
